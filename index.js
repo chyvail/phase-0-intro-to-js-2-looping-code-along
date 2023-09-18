2cddbeae281d54b13560c4d10f2d437c87d81d1e
joshua chivile <joshuachivile@gmail.com>
2023-09-18 08:22:32 +0300

    loops assignment completion

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, events) {
+  const message = [];
+  for (let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+  }
+  return message;
+}
+
+function countDown(n) {
+  let i = n;
+  do {
+    console.log(i);
+    i--;
+  } while (i >= 0);
+}
